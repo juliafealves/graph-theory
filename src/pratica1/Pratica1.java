8b6e90ec7fdbf0241e5baed4a9fb5ecbbaee39f0
juliafealves <juliafealves>
2018-05-06 23:45:06 -0300

    Implementado os métodos para criação de grafo não-orientado ponderado com a biblioteca JGraphT e obter o menor caminho para um gráfico não-orientado ponderado através do algoritmo de Dijkstra.

--- src/pratica1/Pratica1.java
+++ src/pratica1/Pratica1.java
@@ -1,23 +1,32 @@
 package pratica1;
 
 import org.jgrapht.*;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.graph.SimpleWeightedGraph;
 
 import java.util.Arrays;
 
 public class Pratica1 {
 
     public static void main(String[] args){
+        /**
+         * Exemplo de grafo simples para geração de matriz de incidência.
+         */
         String[] vertices = {"a", "b", "c", "d", "e", "f"};
         String[] arestas = {"ab", "ac", "bc", "bd", "be", "ce", "cd", "de", "df", "ef"};
         Graph grafo = criaGrafoSimples(vertices, arestas);
         System.out.println(geraMatrizIncidenciaSimples(grafo));
 
-        String[] vertices1 = {"a", "b", "c", "d", "e"};
-        String[] arestas2 = {"ab", "ad", "bc", "bd", "cd", "de"};
-        Graph grafo1 = criaGrafoSimples(vertices1, arestas2);
-        System.out.println(geraMatrizIncidenciaSimples(grafo1));
+        /**
+         * Exemplo do grafo não-orientado ponderado para obter o menor caminho.
+         */
+        String[] vertices2 = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+        String[] arestas2 = {"AB2", "AG3", "AF7", "BG6", "BC4", "CH2", "CD2", "DH8", "DE1", "EI2", "EF6", "FI5", "IG1", "IH4", "GH3"};
+        SimpleWeightedGraph grafoPonderado = criaGrafoPonderado(vertices2, arestas2);
+        System.out.println(obterMenorCaminho(grafoPonderado, "A", "D"));
     }
 
     /**
@@ -44,7 +53,49 @@ public class Pratica1 {
     }
 
     /**
+     * Cria um grafo do tipo "ponderado não-orientado".
+     *
+     * @param vertices Vértices do grafo.
+     * @param arestas Arestas do grafo no formato "ab1", o número indica o peso da aresta.
+     * @return Retorna um objeto SimpleWeightedGraph
+     */
+    private static SimpleWeightedGraph criaGrafoPonderado(String[] vertices, String[] arestas){
+        SimpleWeightedGraph<String,DefaultWeightedEdge> grafoPonderado = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+
+        for (String vertice : vertices) {
+            grafoPonderado.addVertex(vertice);
+        }
+
+        for (String aresta : arestas){
+            String vertice1 = aresta.substring(0, 1);
+            String vertice2 = aresta.substring(1, 2);
+            int peso = Integer.parseInt(aresta.substring(2));
+            grafoPonderado.setEdgeWeight(grafoPonderado.addEdge(vertice1, vertice2), peso);
+        }
+
+        return grafoPonderado;
+    }
+
+    /**
+     * Retorna o menor caminho entre dois vértices em um grafo ponderado. Utiliza o algoritmo de Dijkstra.
+     *
+     * @param grafoPonderado Grafo do tipo ponderado.
+     * @param verticeOrigem Vértice de origem do caminho.
+     * @param verticeDestino Vértice de destino do caminho.
+     * @return String formatada com informação do caminho e a distância do caminho.
+     */
+    private static String obterMenorCaminho(SimpleWeightedGraph<String, DefaultWeightedEdge> grafoPonderado, String verticeOrigem, String verticeDestino){
+        DijkstraShortestPath<String,DefaultWeightedEdge> menorCaminho = new DijkstraShortestPath <> (grafoPonderado);
+        String caminho = "Menor caminho: " + menorCaminho.getPaths(verticeOrigem).getPath(verticeDestino) + System.lineSeparator();
+        double distancia = menorCaminho.getPathWeight(verticeOrigem, verticeDestino);
+        caminho += "Distância de " + verticeOrigem + " -> " + verticeDestino + ": " + distancia + System.lineSeparator();
+
+        return caminho;
+    }
+
+    /**
      * Gera a matriz de incidência de um grafo simples, contabilizando o grau de cada vértice, o total de grau do grafo.
+     *
      * @param grafo Objeto Graph
      * @return Retorna uma string com a matriz de incidência.
      */
@@ -57,16 +108,12 @@ public class Pratica1 {
             int grau = 0;
 
             for (DefaultEdge aresta: grafo.edgeSet()){
-                String ehIncidente;
-
                 if (aresta.toString().contains(vertice)){
-                    ehIncidente = "   1   ";
+                    linhas[i] = "   1   ";
                     grau++;
                 } else{
-                    ehIncidente = "   0   ";
+                    linhas[i] = "   0   ";
                 }
-
-                linhas[i] = ehIncidente;
                 i++;
             }
             matrizIncidencia += vertice + ":" + Arrays.toString(linhas) + " Grau d(" + vertice + "): " + grau + System.lineSeparator();
